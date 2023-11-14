2b6f5aa73f73559383e033c6e1312852c9350026
Johan Lindqvist <jlt.johanlindqvist@gmail.com>
2023-11-14 17:45:56 +0100

    feat: add stratz graphql api (#7)

---
+++ src/GothmogBot/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using System.Reactive;
+
+namespace GothmogBot;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddStratzGraphQLClient(this IServiceCollection services, string apiKey)
+	{
+		services
+			.AddStratzClient()
+			.ConfigureHttpClient(client =>
+			{
+				client.BaseAddress = new Uri("https://api.stratz.com/graphql");
+				client.Timeout = TimeSpan.FromSeconds(30);
+				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+			});
+
+		return services;
+	}
+}
---
+++ src/GothmogBot/Services/DotaService.cs
@@ -0,0 +1,19 @@
+﻿using GothmogBot.Stratz;
+using StratzClient;
+
+namespace GothmogBot.Services;
+
+public sealed class DotaService
+{
+	private readonly IStratzClient stratzClient;
+
+	public DotaService(IStratzClient stratzClient)
+	{
+		this.stratzClient = stratzClient;
+	}
+
+	public async Task GetDotaMatchAsync(CancellationToken ct)
+	{
+		var matches = await stratzClient.GetPlayerMatches.ExecuteAsync(StratzConstants.BulldogSteamId, ct);
+	}
+}
