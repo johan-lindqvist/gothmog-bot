acff46eee8be504ecf992ac16f7b243175d612e0
Svitkona <svitkona5@gmail.com>
2023-12-23 00:24:07 +0400

    Add constants for points and dank

--- src/GothmogBot/Discord/DiscordConstants.cs
+++ src/GothmogBot/Discord/DiscordConstants.cs
@@ -5,10 +5,19 @@ namespace GothmogBot.Discord;
 public static class DiscordConstants
 {
 	public const ulong BulldogsKappaClubDiscordGuildId = 111772771016515584;
-
 	public const ulong YouTubeTier2RoleId = 777437173904703540;
 	public const ulong TwitchTier3SubscriberRoleId = 707480013406076951;
 	public const ulong CustomMegaCuckRoleId = 948486251315609610;
+	public const ulong DiscordModsRoleId = 117296318052958214;
+	public const ulong RegularRoleId = 345877423142731778;
+	public const ulong DankRoleId = 353238417212964865;
+	public const ulong BasedRoleId = 814422920193245214;
+
+	public const long PointsPerMessage = 10;
+	public const long MaxPointsPerHour = 30;
+	public const long MaxPointsPerDay = 120;
+	public const long MaxPointsPerWeek = 480;
+
 
 	public static ImmutableList<ulong> AllRoles { get; } = ImmutableList.Create(YouTubeTier2RoleId, TwitchTier3SubscriberRoleId, CustomMegaCuckRoleId);
 }
